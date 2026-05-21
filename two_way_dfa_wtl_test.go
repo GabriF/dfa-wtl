8960be780bb728b1dbd3224a4fd11e1378aeafd2
Gabriele Fioco <gabriele@fioco.eu>
2026-05-21 10:43:54 +0200

    Refactor 2dfawtl computation test

--- two_way_dfa_wtl_test.go
+++ two_way_dfa_wtl_test.go
@@ -36,113 +36,8 @@ func TestLinkedListRemoveTail(t *testing.T) {
 	}
 }
 
-func TestComputeNext(t *testing.T) {
-	m := &TwoWayDfaWtl{
-		initialState: 0,
-		delta: [][]int{
-			{1, 2, 6, 7},
-			{7, 7, 3, 7},
-			{7, 7, 4, 7},
-			{5, 7, 7, 7},
-			{7, 5, 7, 7},
-			{7, 7, 7, 0},
-		},
-		tau: [][]bool{
-			{false, false},
-			{true, true},
-			{true, true},
-			{false, false},
-			{false, false},
-			{true, true},
-		},
-		leftMarker:          ']',
-		rightMarker:         '[',
-		alphabetEnumeration: map[rune]int{'a': 0, 'b': 1, '[': 2, ']': 3},
-		stateName:           []string{"q0", "qa", "qb", "pa", "pb", "pr", "accept", "reject"},
-		qrCardinality:       3,
-	}
-	id := NewComputation(m, "abba")
-	ComputeNext(m, id)
-
-	if id.state != 1 || id.stateStr != "qa" || id.Halt {
-		t.FailNow()
-	}
-}
-
-func TestComputeAccept(t *testing.T) {
-	m := &TwoWayDfaWtl{
-		initialState: 0,
-		delta: [][]int{
-			{1, 2, 6, 7},
-			{7, 7, 3, 7},
-			{7, 7, 4, 7},
-			{5, 7, 7, 7},
-			{7, 5, 7, 7},
-			{7, 7, 7, 0},
-		},
-		tau: [][]bool{
-			{false, false},
-			{true, true},
-			{true, true},
-			{false, false},
-			{false, false},
-			{true, true},
-		},
-		leftMarker:          ']',
-		rightMarker:         '[',
-		alphabetEnumeration: map[rune]int{'a': 0, 'b': 1, '[': 2, ']': 3},
-		stateName:           []string{"q0", "qa", "qb", "pa", "pb", "pr", "accept", "reject"},
-		qrCardinality:       3,
-	}
-	id := NewComputation(m, "abba")
-
-	for !id.Halt {
-		ComputeNext(m, id)
-	}
-
-	if (!id.Halt) || id.stateStr != "accept" {
-		t.FailNow()
-	}
-}
-
-func TestComputeReject(t *testing.T) {
-	m := &TwoWayDfaWtl{
-		initialState: 0,
-		delta: [][]int{
-			{1, 2, 6, 7},
-			{7, 7, 3, 7},
-			{7, 7, 4, 7},
-			{5, 7, 7, 7},
-			{7, 5, 7, 7},
-			{7, 7, 7, 0},
-		},
-		tau: [][]bool{
-			{false, false},
-			{true, true},
-			{true, true},
-			{false, false},
-			{false, false},
-			{true, true},
-		},
-		leftMarker:          ']',
-		rightMarker:         '[',
-		alphabetEnumeration: map[rune]int{'a': 0, 'b': 1, '[': 2, ']': 3},
-		stateName:           []string{"q0", "qa", "qb", "pa", "pb", "pr", "accept", "reject"},
-		qrCardinality:       3,
-	}
-	id := NewComputation(m, "abbba")
-
-	for !id.Halt {
-		ComputeNext(m, id)
-	}
-
-	if (!id.Halt) || id.stateStr != "reject" {
-		t.FailNow()
-	}
-}
-
-func TestFromSymbolicDefinition(t *testing.T) {
-	m := FromSymbolDescription(
+func automatons() []*TwoWayDfaWtl {
+	return []*TwoWayDfaWtl{FromSymbolDescription(
 		[]string{"qa", "qb"},
 		[]string{"qc"},
 		"qa",
@@ -169,25 +64,75 @@ func TestFromSymbolicDefinition(t *testing.T) {
 				'a': true,
 			},
 		},
-	)
-
-	t.Log(m)
-
-	id := NewComputation(m, "aababbcc")
-	for !id.Halt {
-		t.Log(id.String())
-		ComputeNext(m, id)
+	),
+		FromSymbolDescription(
+			[]string{"q0", "qa", "qb"},
+			[]string{"pa", "pb", "pr"},
+			"q0",
+			[]rune{'a', 'b'},
+			']',
+			'[',
+			map[string]map[rune]string{
+				"q0": {
+					'[': "accept",
+					'a': "qa",
+					'b': "qb",
+				},
+				"qa": {
+					'[': "pa",
+				},
+				"qb": {
+					'[': "pb",
+				},
+				"pa": {
+					'a': "pr",
+				},
+				"pb": {
+					'b': "pr",
+				},
+				"pr": {
+					']': "q0",
+				},
+			},
+			map[string]map[rune]bool{
+				"qa": {
+					'a': true,
+					'b': true,
+				},
+				"qb": {
+					'a': true,
+					'b': true,
+				},
+				"pr": {
+					'a': true,
+					'b': true,
+				},
+			},
+		),
 	}
-	if id.stateStr != "accept" {
-		t.Fatalf("Expected accept")
+}
+
+func TestAutomatonComputation(t *testing.T) {
+	tests := []struct {
+		m      *TwoWayDfaWtl
+		word   string
+		accept bool
+	}{
+		{automatons()[0], "abaabbcc", true},
+		{automatons()[0], "ababbccc", false},
+
+		{automatons()[1], "ababbbbaba", true},
+		{automatons()[1], "ababbbaba", false},
 	}
 
-	id = NewComputation(m, "aababbc")
-	for !id.Halt {
-		t.Log(id.String())
-		ComputeNext(m, id)
-	}
-	if id.stateStr != "reject" {
-		t.Fatalf("Expected reject")
+	for _, tt := range tests {
+		id := NewComputation(tt.m, tt.word)
+		for !id.Halt {
+			ComputeNext(tt.m, id)
+		}
+		correct := (id.stateStr == "accept" && tt.accept) || (id.stateStr == "reject" && (!tt.accept))
+		if !correct {
+			t.Errorf("Expected %t on input %s with automaton %+v", tt.accept, tt.word, tt.m)
+		}
 	}
 }
