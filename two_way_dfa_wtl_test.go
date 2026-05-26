90ce4152e8dce7610fbc6d11ac770986f8a2607a
Gabriele Fioco <gabriele@fioco.eu>
2026-05-26 17:07:16 +0200

    Remove duplicated linked list test and make test black-box

--- two_way_dfa_wtl_test.go
+++ two_way_dfa_wtl_test.go
@@ -1,40 +1,10 @@
-package gotwodfawtl
+package gotwodfawtl_test
 
-import "testing"
+import (
+	"testing"
 
-func TestLinkedListOneElement(t *testing.T) {
-	ll := fromStringDLL("a")
-	if ll.String() != "a" {
-		t.FailNow()
-	}
-}
-
-func TestLinkedListRemove(t *testing.T) {
-	ll := fromStringDLL("aba")
-	toRemove := ll.head.next
-	ll.remove(toRemove)
-	if ll.String() != "aa" {
-		t.FailNow()
-	}
-}
-
-func TestLinkedListRemoveHead(t *testing.T) {
-	ll := fromStringDLL("aba")
-	toRemove := ll.head
-	ll.remove(toRemove)
-	if ll.String() != "ba" {
-		t.FailNow()
-	}
-}
-
-func TestLinkedListRemoveTail(t *testing.T) {
-	ll := fromStringDLL("aba")
-	toRemove := ll.tail
-	ll.remove(toRemove)
-	if ll.String() != "ab" {
-		t.FailNow()
-	}
-}
+	. "github.com/GabriF/dfa-wtl"
+)
 
 func automatons() []*TwoWayDfaWtl {
 	return []*TwoWayDfaWtl{FromSymbolDescription(
