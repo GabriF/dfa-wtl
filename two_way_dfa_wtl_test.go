140a1b3b2574a5d915a951f0b3391c0f4779df25
Gabriele Fioco <gabriele@fioco.eu>
2026-05-27 15:11:53 +0200

    Add 2dfawtl computation benchmark

--- two_way_dfa_wtl_test.go
+++ two_way_dfa_wtl_test.go
@@ -1,6 +1,8 @@
 package gotwodfawtl_test
 
 import (
+	"math/rand"
+	"strings"
 	"testing"
 
 	. "github.com/GabriF/dfa-wtl"
@@ -103,6 +105,45 @@ func TestAutomatonComputation(t *testing.T) {
 		correct := (id.Accept && tt.accept) || ((!id.Accept) && (!tt.accept))
 		if !correct {
 			t.Errorf("Expected %t on input %s with automaton %+v", tt.accept, tt.word, tt.m)
+		}
+	}
+}
+
+func BenchmarkAutomatonComputationBenchmark(b *testing.B) {
+	m := automatons()[0]
+	n := 100000000
+
+	for b.Loop() {
+		b.StopTimer()
+
+		str := &strings.Builder{}
+		a_count, b_count := 0, 0
+		for range n * 2 {
+			if a_count == n {
+				str.WriteRune('b')
+			} else if b_count == n {
+				str.WriteRune('a')
+			} else if rand.Intn(2) == 0 {
+				str.WriteRune('a')
+				a_count++
+			} else {
+				str.WriteRune('b')
+				b_count++
+			}
+		}
+		for range n - 1 {
+			str.WriteRune('c')
+		}
+		w := str.String()
+
+		b.StartTimer()
+
+		id := NewComputation(m, w)
+		for !id.Halt {
+			ComputeNext(m, id)
+		}
+		if !id.Accept {
+			b.FailNow()
 		}
 	}
 }
